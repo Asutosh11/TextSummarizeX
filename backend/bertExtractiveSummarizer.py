2bb7d32a0f6944c4582b571baacf7bd46ac81d42
Asutosh11 <panda.asutosh04@gmail.com>
2020-09-09 14:00:32 +0530

    API changes. Give option in all APIs for setting fraction_of_original_text in summary

--- backend/bertExtractiveSummarizer.py
+++ backend/bertExtractiveSummarizer.py
@@ -1,9 +1,10 @@
 from summarizer import Summarizer
 
-def summarize(text):
+def summarize(text, fraction_of_original_text):
 
+    fraction_of_original_text = float(fraction_of_original_text)
     model = Summarizer()
-    result = model(text, min_length=40, ratio=0.1)
+    result = model(text, min_length=40, ratio=fraction_of_original_text)
     fullOutput = ''.join(result)
 
     return fullOutput
