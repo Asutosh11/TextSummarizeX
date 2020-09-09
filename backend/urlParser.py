61b58260e8135ebf9f50d543f3587b9612bdf593
Asutosh11 <panda.asutosh04@gmail.com>
2020-09-09 13:08:38 +0530

    make url parser better - introduce a new better library

--- backend/urlParser.py
+++ backend/urlParser.py
@@ -1,36 +1,9 @@
-from bs4 import BeautifulSoup
-from bs4.element import Comment
-from urllib.request import Request, urlopen
-
-# def tag_visible(element):
-#     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]', 'ul', 'li', 'a', 'h1', 'ol', 'footer']:
-#         return False
-#     if isinstance(element, Comment):
-#         return False
-#     return True
-
-def tag_visible(element):
-    if element.parent.name in ['p', 'span']:
-        return True
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]', 'ul', 'li', 'a', 'h1', 'ol', 'footer']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return False
+import trafilatura
 
 
-# this is the function that has to be called for get text from a webpage URL
-# Other functions in this file are just helper functions
 def getTextFromURL(url):
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = Request(url,headers=hdr)
     try:
-        page = urlopen(req)
-        soup = BeautifulSoup(page)
-        texts = soup.prettify()
-        texts = soup.findAll(text=True)
-        visible_texts = filter(tag_visible, texts)  
-        final_text = u" ".join(t.strip() for t in visible_texts)
-        return (final_text.lstrip())
+       downloaded = trafilatura.fetch_url(url)
+       return(trafilatura.extract(downloaded))
     except:
         return""  
