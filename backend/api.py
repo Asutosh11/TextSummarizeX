a3052d8799d2deede4f2b215f75925f130675038
Asutosh Panda <panda.asutosh04@gmail.com>
2020-10-12 17:41:24 +0530

    correct the dummy route added in previous commit

--- backend/api.py
+++ backend/api.py
@@ -21,6 +21,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
            
 @app.route('/', methods=['GET', 'POST'])
+def test():
     return "works fine"
            
 
